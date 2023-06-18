967b34d2df963691db800270bbcc5750f34ac36d
Jovany7068 <jovitanyanwu99@gmail.com>
2023-06-18 15:02:29 -0700

    5 prints

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	char alp[52] =
 	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
@@ -16,4 +15,4 @@ int main(void)
 	{
 	putchar('\n');
 	return (0);
-	}
+}
