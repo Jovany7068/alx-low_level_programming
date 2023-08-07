237d3553d80a1df435426a8c8d72492907d65525
Jovany7068 <jovitanyanwu99@gmail.com>
2023-08-07 08:50:00 -0700

    bit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -14,26 +14,19 @@ int main(int argc, char *argv[])
 int bytes, i;
 char *arr;
 
-
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-
-
 bytes = atoi(argv[1]);
-
 
 if (bytes < 0)
 {
 printf("Error\n");
 exit(2);
 }
-
-
 arr = (char *)main;
-
 
 for (i = 0; i < bytes; i++)
 {
@@ -42,7 +35,7 @@ if (i == bytes - 1)
 printf("%02hhx\n", arr[i]);
 break;
 }
-printf("%02hhx\n", arr[i]);
+printf("%02hhx ", arr[i]);
 }
 return (0);
 }
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+0x14. C - Bit manipulation
+AND operator, OR, <<, >>, ~ and XOR operators
